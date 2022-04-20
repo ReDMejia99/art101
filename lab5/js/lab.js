df253b36b2af2a84bdf9ce0986b3ebddd95e8ade
ReDMejia99 <redmejia@ucsc.edu>
2022-04-20 11:21:00 -0700

    Lab 5 upload

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+// Summary: Lab 5 JavaScript for Data Types and Variables
+// Description: In this lab we are practicing coding with variables and then inputting them into our JavaScript code.
+// author Reece Mejia.
+// date:April 20,2022.
+
+
+// Define Variables
+make = "Kia";
+model ="Soul";
+color ="gray";
+year = 2015;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
