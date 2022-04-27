71faa98bc25b23e0ac736b27df0e0747d9f84c97
ReDMejia99 <redmejia@ucsc.edu>
2022-04-27 02:49:08 -0700

    Home Page links updated

---
+++ lab7/js/lab.js
@@ -0,0 +1,26 @@
+// Title: Lab 7 Functions.
+// Description: In this lab we are practicing using JavaScript arrays and objects.
+// author: Reece Mejia.
+// date:April 25,2022.
+
+
+// Define variables
+myTransport = ["gray Kia", "longboard", "bus"];
+
+myMainRide = {
+color: "gray",
+make: "Kia",
+model: "Soul",
+year: 2015,
+age: 7
+}
+
+
+
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
