5af0f5533eddcf05005788aadcdbf0893d7f4784
ReDMejia99 <redmejia@ucsc.edu>
2022-04-27 10:40:57 -0700

    lab 7 final upload

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,26 +1,24 @@
 // Title: Lab 7 Functions.
-// Description: In this lab we are practicing using JavaScript arrays and objects.
+// Description: In this lab my partner and I experimented with functions in order to create a window login for our website.
 // author: Reece Mejia.
-// date:April 25,2022.
-
-
-// Define variables
-myTransport = ["gray Kia", "longboard", "bus"];
-
-myMainRide = {
-color: "gray",
-make: "Kia",
-model: "Soul",
-year: 2015,
-age: 7
+// date:April 27,2022.
+// sortPassword- a function that takes user input and sorts the letters
+// of their chosen password.
+function sortPassWord() {
+  var passWord = window.prompt ("Hi. Enter your password to proceed.");
+  console.log("passWord =", passWord);
+  // split string to array
+  var passWordArray = passWord.split('');
+  console.log("passWordArray =", passWordArray);
+  // sort the array
+  var passWordArraySort = passWordArray.sort();
+  console.log("passWordArraySort =", passWordArraySort);
+  // join array back to a string
+  var passWordSorted = passWordArraySort.join('');
+  console.log("passWordSorted = ", passWordSorted);
+  //passWord.toLower().split("").sort().join("")
+  return passWordSorted;
 }
-
-
-
-
-// output
-document.writeln("Make: " + make + "<br>");
-document.writeln("Model: " + model + "<br>");
-document.writeln("Color: " + color + "<br>");
-document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + " years<br>");
+  // output
+document.writeln("Welcome to our lab here is your password for the day(scrambled up for your privacy): ",
+  sortPassWord(),"</br>");
